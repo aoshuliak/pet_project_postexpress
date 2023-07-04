6d0b3bff67cb90786da8d82c5a5cf049e8e134ab
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-04 23:46:51 +0300

    Initializing project files

---
+++ src/main/java/com/postexpress/Postrexpress/PostrexpressApplication.java
@@ -0,0 +1,13 @@
+package com.postexpress.Postrexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostrexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostrexpressApplication.class, args);
+	}
+
+}
