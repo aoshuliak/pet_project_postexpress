4407a6cdf360438a012bfe3d814c522fd5f3b909
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-06 22:20:56 +0300

    Added repo for Package entity

--- src/main/java/com/postexpress/Postrexpress/repository/PackageRepository.java
+++ src/main/java/com/postexpress/Postrexpress/repository/PackageRepository.java
@@ -1,2 +1,11 @@
-package com.postexpress.Postrexpress.repository;public interface PackageRepository {
+package com.postexpress.Postrexpress.repository;
+
+import com.postexpress.Postrexpress.model.Package;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PackageRepository extends JpaRepository<Package, Long> {
+    Package getPackageByAddresser_Email(String email);
+    Package getPackageByRecipient_Email(String email);
 }
