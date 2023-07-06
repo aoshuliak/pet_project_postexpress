1be03d97fc082487914139d45bd875ccd745fab6
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-06 22:21:25 +0300

    Added the service with crud methods for Package entity

--- src/main/java/com/postexpress/Postrexpress/service/PackageService.java
+++ src/main/java/com/postexpress/Postrexpress/service/PackageService.java
@@ -1,2 +1,14 @@
-package com.postexpress.Postrexpress.service;public interface PackageService {
+package com.postexpress.Postrexpress.service;
+
+import com.postexpress.Postrexpress.model.Package;
+import com.postexpress.Postrexpress.model.User;
+
+import java.util.List;
+
+public interface PackageService {
+    Package create(Package pack);
+    Package readById(long id);
+    Package update(Package pack);
+    void delete(long id);
+    List<Package> getAll();
 }
