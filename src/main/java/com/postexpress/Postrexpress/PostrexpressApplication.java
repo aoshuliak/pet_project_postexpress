04f1af345edd0d664b3f1cde01b0a94d064f9643
mrRibka <egorkondratov@gmail.com>
2023-07-06 18:13:57 +0300

    security temporary disabled

--- src/main/java/com/postexpress/Postrexpress/PostrexpressApplication.java
+++ src/main/java/com/postexpress/Postrexpress/PostrexpressApplication.java
@@ -2,8 +2,9 @@ package com.postexpress.Postrexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PostrexpressApplication {
 
 	public static void main(String[] args) {
