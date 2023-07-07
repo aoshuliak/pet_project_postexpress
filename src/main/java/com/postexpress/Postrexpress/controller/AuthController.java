76c9c6f403865b142208887990f9aaf0b0bc6928
mrRibka <egorkondratov@gmail.com>
2023-07-07 13:11:17 +0300

    registration added

---
+++ src/main/java/com/postexpress/Postrexpress/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.postexpress.Postrexpress.controller;
+
+import com.postexpress.Postrexpress.dto.UserDTO;
+import com.postexpress.Postrexpress.model.Role;
+import com.postexpress.Postrexpress.model.User;
+import com.postexpress.Postrexpress.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Validated @ModelAttribute("user") UserDTO user,
+                         BindingResult result) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        user.setRole(Role.USER);
+        User newUser = userService.create(UserDTO.transformToEntity(user));
+        return "redirect:/";
+    }
+}
