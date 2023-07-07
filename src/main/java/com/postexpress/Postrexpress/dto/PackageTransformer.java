e638a3fda9e262d7c96d4e1880c9336570c66831
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-07 15:25:14 +0300

    Added transformer entity/dto for Package

--- src/main/java/com/postexpress/Postrexpress/dto/PackageTransformer.java
+++ src/main/java/com/postexpress/Postrexpress/dto/PackageTransformer.java
@@ -1,2 +1,30 @@
-package com.postexpress.Postrexpress.dto;public class PackageTransformer {
+package com.postexpress.Postrexpress.dto;
+
+import com.postexpress.Postrexpress.model.Package;
+import com.postexpress.Postrexpress.model.Status;
+import com.postexpress.Postrexpress.model.User;
+
+public class PackageTransformer {
+    public static PackageDTO convertToDto(Package pack) {
+        return new PackageDTO(
+                pack.getId(),
+                pack.getName(),
+                pack.getDescription(),
+                pack.getRecipient(),
+                pack.getAddresser(),
+                pack.getStatus()
+        );
+    }
+
+    public static Package convertToEntity(PackageDTO packageDTO, User recipient, User addresser, Status status) {
+        Package pack = new Package();
+        pack.setId(packageDTO.getId());
+        pack.setName(packageDTO.getName());
+        pack.setDescription(packageDTO.getDescription());
+        pack.setRecipient(recipient);
+        pack.setAddresser(addresser);
+        pack.setStatus(status);
+        return pack;
+    }
+
 }
