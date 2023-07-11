9de82663866e423438d30d4c8907f8858c3f8c1a
mrRibka <egorkondratov@gmail.com>
2023-07-11 23:52:29 +0300

    user controller implemented

---
+++ src/main/java/com/postexpress/Postrexpress/controller/UserController.java
@@ -0,0 +1,80 @@
+package com.postexpress.Postrexpress.controller;
+
+import com.postexpress.Postrexpress.dto.UserDTO;
+import com.postexpress.Postrexpress.model.Role;
+import com.postexpress.Postrexpress.model.User;
+import com.postexpress.Postrexpress.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}/read")
+    public String read(@PathVariable long id,
+                       Model model) {
+        User user = userService.readById(id);
+        model.addAttribute("user", user);
+        return "user-info";
+    }
+
+    @GetMapping("/{id}/update")
+    public String update(@PathVariable long id,
+                         Model model) {
+        User user = userService.readById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "update-user";
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable long id,
+                         Model model,
+                         @Validated @ModelAttribute("user") UserDTO user,
+                         @RequestParam("role") Role role,
+                         BindingResult result) {
+        User oldUser = userService.readById(id);
+        if (result.hasErrors()) {
+            user.setRole(oldUser.getRole());
+            model.addAttribute("user", user);
+            model.addAttribute("roles", Role.values());
+            return "update-user";
+        }
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        if (oldUser.getRole().equals(Role.USER)) {
+            user.setRole(oldUser.getRole());
+        } else {
+            user.setRole(role);
+        }
+        userService.update(UserDTO.transformToEntity(user));
+        return "redirect:/users/" + id + "/read";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable("id") long id){
+        userService.delete(id);
+        return "redirect:/users/all";
+    }
+
+    @GetMapping("/all")
+    public String getAll(Model model) {
+        model.addAttribute("users", userService.getAll());
+        return "users-list";
+    }
+}
