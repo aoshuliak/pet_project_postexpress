36f66adc37ae72bba8b63e628df59c831de4861a
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-13 00:10:26 +0300

    Added query for getbyuserid method

--- src/main/java/com/postexpress/Postrexpress/repository/PackageRepository.java
+++ src/main/java/com/postexpress/Postrexpress/repository/PackageRepository.java
@@ -2,10 +2,17 @@ package com.postexpress.Postrexpress.repository;
 
 import com.postexpress.Postrexpress.model.Package;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PackageRepository extends JpaRepository<Package, Long> {
+
+    @Query(value = "select * from packages where id = ?1", nativeQuery = true)
+    List<Package> getByUserId(long userId);
+
     Package getPackageByAddresser_Email(String email);
     Package getPackageByRecipient_Email(String email);
 }
