26f5b307a1928f0f9df1c260c9f1b75b5bdb7d34
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-18 01:05:35 +0300

    Adding Optional method

--- src/main/java/com/postexpress/Postrexpress/service/UserService.java
+++ src/main/java/com/postexpress/Postrexpress/service/UserService.java
@@ -4,6 +4,7 @@ package com.postexpress.Postrexpress.service;
 import com.postexpress.Postrexpress.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     User create(User user);
@@ -11,7 +12,9 @@ public interface UserService {
     User update(User user);
     void delete(long id);
     User findByEmail(String email);
+    Optional<User> findByUserEmail(String email);
     List<User> getAll();
+    boolean authenticateUser(String username, String password);
     boolean userExistsById(long id);
 }
 
