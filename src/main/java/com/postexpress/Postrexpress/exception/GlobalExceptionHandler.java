4abf5167faa182d28034743bf12eae3db30b8032
mrRibka <egorkondratov@gmail.com>
2023-07-17 22:22:08 +0300

    class created

---
+++ src/main/java/com/postexpress/Postrexpress/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.postexpress.Postrexpress.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+}
