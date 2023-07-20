3af39be07efd11a056e210c8f3c42a69479b65ca
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-20 18:36:49 +0300

    Updated methods for implementation

--- src/main/java/com/postexpress/Postrexpress/service/UserService.java
+++ src/main/java/com/postexpress/Postrexpress/service/UserService.java
@@ -2,7 +2,10 @@ package com.postexpress.Postrexpress.service;
 
 
 import com.postexpress.Postrexpress.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
