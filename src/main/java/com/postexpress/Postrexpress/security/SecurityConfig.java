191a2b7db507e4febefbf56a88195d04a8d765a7
Andrii Shuliak <andrij.shuljak@gmail.com>
2023-07-25 23:23:49 +0300

    Disabling csrf in the security configuration

--- src/main/java/com/postexpress/Postrexpress/security/SecurityConfig.java
+++ src/main/java/com/postexpress/Postrexpress/security/SecurityConfig.java
@@ -41,9 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/home", "/change", "/register", "/").permitAll()
-//                .antMatchers("/users/all/").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
